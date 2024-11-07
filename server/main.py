84658e67b4db4a010115fd1446be3805a9979ed1
datmemerboi <shravanssd@gmail.com>
2024-11-08 02:43:34 +1100

    refactor: Export csv with date

--- server/main.py
+++ server/main.py
@@ -4,6 +4,8 @@ from sys import argv
 
 from requests_cache import CachedSession
 
+from config.constants import RESULTS_FOLDER
+from utils.date import make_prefix
 from transform.product import create_product_df
 from transform.price import create_price_df
 from transform.dataframe import product_list_to_df, save_csv
@@ -19,7 +21,7 @@ for i in range(1, 5):
     product_list.extend(
         pick_products(
             Woolworths.category(
-                session=session, category_name="half-price", page=i, size=30
+                session=session, category_name="HalfPrice", page=i, size=30
             )
         )
     )
@@ -28,15 +30,10 @@ print()
 price_df = create_price_df(product_list, Woolworths.vendor)
 product_df = create_product_df(product_list)
 
-price_csv_path = path.abspath(path.join(__file__, "..", "..", "data", "price_result.csv"))
+price_csv_name = f"{make_prefix()}price-result.csv"
+price_csv_path = path.abspath(path.join(RESULTS_FOLDER, price_csv_name))
 save_csv(price_df, price_csv_path)
-product_csv_path = path.abspath(path.join(__file__, "..", "..", "data", "product_result.csv"))
+
+product_csv_name = f"{make_prefix()}product-result.csv"
+product_csv_path = path.abspath(path.join(RESULTS_FOLDER, product_csv_name))
 save_csv(product_df, product_csv_path)
-
-# result_csv_path = path.abspath(path.join(__file__, "..", "..", "data", "result.csv"))
-# parsed_json_path = path.abspath(
-#     path.join(__file__, "..", "..", "data", "parsed_sample.json")
-# )
-
-# json.dump(product_list, open(parsed_json_path, "w"), indent=2)
-# save_csv(product_list_to_df(product_list), result_csv_path)
