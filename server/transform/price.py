4343d12b6461c7e1f7883ab316542ac3e5366422
datmemerboi <shravanssd@gmail.com>
2024-11-12 02:29:35 +1100

    fix: transform based on new WoolworthsProduct

--- server/transform/price.py
+++ server/transform/price.py
@@ -1,30 +1,20 @@
-import pandas as pd
-
-from utils.date import get_previous_wednesday, get_upcoming_tuesday
+from pandas import json_normalize, DataFrame
 
 
-def create_price_df(products: list[dict], vendor: str) -> pd.DataFrame:
-    df = pd.json_normalize(products)
-
-    price_df = df[
-        ["barcode", "price", "previous_price", "cost_per_unit", "cost_per_unit_measure"]
-    ].copy()
-    price_df["vendor"] = vendor
-    price_df["viewed_date"] = get_previous_wednesday()
-    price_df["tentative_end_date"] = get_upcoming_tuesday()
+def create_price_df(products: list[dict]) -> DataFrame:
+    df = json_normalize(products)
 
     # Re-order columns
-    price_df = price_df[
+    return df[
         [
             "barcode",
             "vendor",
             "price",
-            "previous_price",
             "viewed_date",
             "tentative_end_date",
             "cost_per_unit",
             "cost_per_unit_measure",
+            "previous_price",
+            "url",
         ]
     ]
-
-    return price_df
