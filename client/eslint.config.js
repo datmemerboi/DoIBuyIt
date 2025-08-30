ab9a8cbe0a33c6f8fe968e78d3792bda43209403
Shravan Dave <shravanssd@gmail.com>
2025-08-30 22:41:56 +1000

    dep: update yarn and eslint packages

---
+++ client/eslint.config.js
@@ -0,0 +1,63 @@
+import js from "@eslint/js";
+import pluginImport from "eslint-plugin-import";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import pluginSimpleImportSort from "eslint-plugin-simple-import-sort";
+import pluginUnusedImports from "eslint-plugin-unused-imports";
+import globals from "globals";
+
+export default [
+  { ignores: ["dist", ".pnp.cjs", ".pnp.loader.mjs", ".yarn"] },
+  {
+    files: ["**/*.{ts,tsx,js,jsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        ecmaFeatures: { jsx: true },
+        sourceType: "module"
+      }
+    },
+    settings: { react: { version: "18.3" } },
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      "simple-import-sort": pluginSimpleImportSort,
+      import: pluginImport,
+      "unused-imports": pluginUnusedImports
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs["jsx-runtime"].rules,
+      ...reactHooks.configs.recommended.rules,
+      "react/jsx-no-target-blank": "off",
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true }
+      ],
+      semi: ["error", "always"],
+      "react/prop-types": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "error",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_"
+        }
+      ],
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "import/first": "error",
+      "import/newline-after-import": "error",
+      "import/no-duplicates": "error"
+    }
+  },
+  eslintPluginPrettierRecommended
+];
