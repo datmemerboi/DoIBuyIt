7cbdc6f20c9e22c96ea9c593646e6829a7956b3c
Shravan Dave <shravanssd@gmail.com>
2025-08-31 00:22:20 +1000

    fix: main data extraction & load

--- server/main.py
+++ server/main.py
@@ -1,12 +1,6 @@
-import json
 import django
 import requests
-from sys import argv
 from os import path, environ
-
-
-environ.setdefault("DJANGO_SETTINGS_MODULE", "doibuyit.settings")
-django.setup()
 
 from config.constants import RESULTS_FOLDER
 from load.product import load_product_data
@@ -18,6 +12,9 @@ from transform.dataframe import save_csv
 from transform.woolworths import pick_products
 from spiders.woolworths import Woolworths
 
+environ.setdefault("DJANGO_SETTINGS_MODULE", "doibuyit.settings")
+django.setup()
+
 
 session = requests.Session()
 session.get(
@@ -25,17 +22,19 @@ session.get(
     headers={
         "Accept": "application/json, */*",
         "Content-Type": "application/json",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+        + "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 "
+        + "Safari/537.",
     },
 )
 
 product_list = []
-for i in range(1, 20):
+for page in range(1, 40):
     print(".", end="")
     product_list.extend(
         pick_products(
             Woolworths.category(
-                session=session, category_name="HalfPrice", page=i, size=30
+                session=session, category_name="HalfPrice", page=page, size=30
             )
         )
     )
@@ -43,6 +42,14 @@ print()
 
 price_df = create_price_df(product_list)
 product_df = create_product_df(product_list)
+
+# Ensure RESULTS_FOLDER exists
+if not path.exists(RESULTS_FOLDER):
+    import os
+
+    print("Creating folders for: ", RESULTS_FOLDER)
+
+    os.makedirs(RESULTS_FOLDER)
 
 price_csv_name = f"{make_prefix()}price-result.csv"
 price_csv_path = path.abspath(path.join(RESULTS_FOLDER, price_csv_name))
