ab4f3c8da5cee28665f3256b5d051079614ba2ca
Shravan Dave <shravanssd@gmail.com>
2025-08-31 00:55:25 +1000

    feat: change Price to monospace font

--- client/components/ProductCard.tsx
+++ client/components/ProductCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Button from "@/components/Button";
+import { Red_Hat_Mono } from "next/font/google";
 
 interface ProductCardProps {
   title: string;
@@ -10,6 +11,12 @@ interface ProductCardProps {
   image?: string;
 }
 
+const monospaceFont = Red_Hat_Mono({
+  weight: "600",
+  style: "normal",
+  subsets: ["latin"]
+});
+
 const cardStyle: string =
   "w-11/12 bg-white lg:w-3/12 lg:bg-white sm:w-3/12 sm:bg-red-100 md:w-3/12 md:bg-yellow-200 bg-gray-100 rounded-lg shadow-lg p-4 m-4 flex flex-col";
 const imageContainerStyle: string =
@@ -18,7 +25,7 @@ const cardImageStyle: string = "object-contain lg:h-full";
 const textContainerStyle: string = "flex flex-col";
 const productTitleStyle: string = "text-base font-bold md:text-xl";
 const productVendorStyle: string = "text-m text-gray-500";
-const productPriceStyle: string = "p-2 text-2xl font-medium";
+const productPriceStyle: string = `p-2 text-2xl font-medium ${monospaceFont.className}`;
 
 export default function ProductCard({
   title,
@@ -32,7 +39,7 @@ export default function ProductCard({
   const renderPrice = () => {
     if (price) {
       // TODO: Indicate vendor-price rise/fall
-      return <p className={productPriceStyle}>${price}</p>;
+      return <span className={productPriceStyle}>${price.toFixed(2)}</span>;
     }
   };
   const renderImage = () => {
@@ -54,9 +61,13 @@ export default function ProductCard({
   return (
     <div className={cardStyle}>
       {renderImage()}
-      {renderText()}
-      {renderVendor()}
-      <Button text={"Open this product"} wide={true} />
+      <div className="flex flex-col flex-1 justify-between">
+        <div className="flex-1">
+          {renderText()}
+          {renderVendor()}
+        </div>
+        <Button text={"Open this product"} wide={true} />
+      </div>
     </div>
   );
 }
